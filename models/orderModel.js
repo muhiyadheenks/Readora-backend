d964ae7c50fcfe42b2972dc0866ba61f91001ae3
muhiyadheen <muhiyadheensj@gmail.com>
2026-05-26 16:03:15 +0530

    fix

--- models/orderModel.js
+++ models/orderModel.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const orderSchema = new mongoose.Schema({
     orderId: {
         type: String,
-        unique: true
+        unique: true,
+        default: () => new mongoose.Types.ObjectId().toString()
     },
     userId: {
         type: mongoose.Schema.Types.ObjectId,
